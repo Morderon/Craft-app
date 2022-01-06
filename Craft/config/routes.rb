aae40092c44192edd4e819af961a71747e4771d1
Eric Klempnauer <me@eric-klempnauer.com>
2022-01-05 19:30:39 -0700

    fix: spelling error in generated recipe files

--- Craft/config/routes.rb
+++ Craft/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-    get '/recipes' => 'recipec#index'
-    get '/recipes/skill' => 'recipec#indexskill', as: :skillindex
-    get '/recipes/skill/:Skill' => 'recipec#showskill', as: :skill
+    get '/recipes' => 'recipes#index'
+    get '/recipes/skill' => 'recipes#indexskill', as: :skillindex
+    get '/recipes/skill/:Skill' => 'recipes#showskill', as: :skill
     get '/recipes/input/:Name(/skill/:Skill)(/:any)'    => 'inputs#show', as: :input
     #get '/recipes/input/:Name'    => 'inputs#show', as: :input
-    get '/recipes/:ID' => 'recipec#show', as: :recipe
+    get '/recipes/:ID' => 'recipes#show', as: :recipe
     get '/json' => 'json#index', format: 'json'
-    get '*path' => 'recipec#index'
-    root to: 'recipec#index'
+    get '*path' => 'recipes#index'
+    root to: 'recipes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
