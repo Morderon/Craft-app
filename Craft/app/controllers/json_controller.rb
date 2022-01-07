8dba3a05af6b35f02c28667b29458593c9b94d60
Eric Klempnauer <me@eric-klempnauer.com>
2022-01-07 15:53:48 -0700

    feature: revise json to structured data

--- Craft/app/controllers/json_controller.rb
+++ Craft/app/controllers/json_controller.rb
@@ -2,7 +2,39 @@ class JsonController < ApplicationController
   caches_action :index, expires_in: 1.hour
 
   def index
-    #SELECT GROUP_CONCAT(CONCAT('{', '"id": ',r.ID,',',' "Craft": ',Skill,',',' "Category": "',c.name,'",',' "Name": "',r.Name,'",',' "Value": ',Value,',',' "SubRace": ',SubRace,',',' "Class": ',Class,',',' "Level": ',Class_Level,',',' "DC": ',DC,',',' "Points": ',Points,',',' "Settings": ',Settings,',',' "Tag": "',Placeable_Tag,'",',' "Output": ',CONCAT('[', coalesce((select GROUP_CONCAT(CONCAT( '{','"Name": "',Name,'",',' "Qty": ',Qty, '}'
-    @json = Recipe.select("md_cr_recipes.ID, md_cr_recipes.Skill AS Craft, c.name AS Category, md_cr_recipes.Name, md_cr_recipes.Value, md_cr_recipes.SubRace, md_cr_recipes.Class, md_cr_recipes.Class_Level AS Level, md_cr_recipes.DC, md_cr_recipes.Points, md_cr_recipes.Settings, md_cr_recipes.Placeable_Tag AS Tag").joins("INNER JOIN md_cr_category AS c ON c.id=md_cr_recipes.category").where("Settings & 1 != 1").order(:id).to_json(:include => [{:output => {:only => [:Name, :Qty]}}, {:input => {:only => [:Name, :Qty]}}, {:feat => {:only => :Feat}}, {:skill => {:only => [:Skill, :Rank]}}])
+    # Load all recipes and preload relational models
+    @all = Recipe
+      .includes(:output, :input, :feat, :skill)
+      .joins("INNER JOIN md_cr_category ON md_cr_category.id=md_cr_recipes.category")
+      .where("Settings & 1 != 1")
+      .references(:output, :input, :feat, :skill)
+
+    @structured = {
+      'byCraft' => {},
+      'byID' => {},
+    }
+
+    @all.each do |recipe|
+      # Handle creating or adding nested hashes
+      if !@structured['byCraft'].has_key? recipe.Skill
+        @structured['byCraft'][recipe.Skill] = { 'byID' => {}, 'byCategory' => {} }
+      end
+
+      if !@structured['byCraft'][recipe.Skill]['byCategory'].has_key? recipe.category
+        @structured['byCraft'][recipe.Skill]['byCategory'][recipe.category] = {}
+      end
+
+      # Insert recipe into nested and root hashes
+      @structured['byID'].store(recipe.ID, recipe)
+      @structured['byCraft'][recipe.Skill]['byID'].store(recipe.ID, recipe)
+      @structured['byCraft'][recipe.Skill]['byCategory'][recipe.category].store(recipe.ID, recipe)
+    end
+
+    render json: @structured.to_json(:include => [
+        {:output => {:only => [:Name, :Qty]}},
+        {:input => {:only => [:Name, :Qty]}},
+        {:feat => {:only => :Feat}},
+        {:skill => {:only => [:Skill, :Rank]}}
+      ])
   end
 end
